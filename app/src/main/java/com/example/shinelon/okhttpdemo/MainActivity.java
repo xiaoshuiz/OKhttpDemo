26893e1eef725d432e05414db78b1b618b13f243
xiaoshui <shui_huachun@126.com>
2017-06-06 16:06:15 +0800

    update webview and link source

--- app/src/main/java/com/example/shinelon/okhttpdemo/MainActivity.java
+++ app/src/main/java/com/example/shinelon/okhttpdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.shinelon.okhttpdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewStub;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (((int)(Math.random()*100)&0x01)==0) {
+            ViewStub stub =(ViewStub)findViewById(R.id.viewStub);
+            stub.inflate();
+            WebView webView=(WebView) findViewById(R.id.webview);
+        }
     }
 }
